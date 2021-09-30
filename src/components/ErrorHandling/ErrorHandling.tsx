9379a684381deca73902dc92af5ba35d4702a083
Fara Akhatova <faraakhatova@gmail.com>
2021-09-29 20:19:28 -0400

    comment out error icon

--- src/components/ErrorHandling/ErrorHandling.tsx
+++ src/components/ErrorHandling/ErrorHandling.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import errorIcon from '../../assets/error.png';
+// import errorIcon from '../../assets/error.png';
 import './ErrorHandling.css';
 
 interface IErrorProps{
@@ -10,7 +10,7 @@ interface IErrorProps{
 const ErrorHandling:React.FC<IErrorProps> = ({errorMessage}) => {
   return (
     <section className="error-display" >
-      <img src={errorIcon} alt="error icon" className="error-img" />
+      {/* <img src={errorIcon} alt="error icon" className="error-img" /> */}
       <p>
         There was an issue!
       </p>
